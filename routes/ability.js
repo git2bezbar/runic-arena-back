160f510684fed5ceb46f33a9731b7fd419f684b6
LeonardPaillet <Tactic22paill@gmail.com>
2023-05-06 23:37:26 +0200

    ability affichage

--- routes/ability.js
+++ routes/ability.js
@@ -3,9 +3,98 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 var router = express.Router();
 
-/* GET skills listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/* GET ability listing. */
+router.get('/', async function(req, res, next) {
+  try{
+    const abilities = await prisma.ability.findMany()
+    res.send(abilities)
+  }catch(error){
+    console.log(error.message)
+    res.status(500).json({
+      message:"Internal Server Error"
+    })
+  }
 });
 
+/* Affichage unitaire */
+router.get('/:id', async function(req, res, next) {
+  try{
+    const id = parseInt(req.params.id)
+    const ability = await prisma.ability.findUnique({
+      where:{
+        id:id
+      }
+    })
+    res.send(ability)
+  }catch(error){
+    console.log(error.message)
+    res.status(500).json({
+      message:"Internal Server Error"
+    })
+  }
+});
+
+/* Mise à jour type */
+router.post('/:id', async function(req, res, next) {
+  try{
+    const {name, description} = req.body
+    const id = parseInt(req.params.id)
+    const updatedType = await prisma.type.update({
+      where:{
+        id:id
+      },
+      data:{
+        name,
+        description
+      }
+    })
+    
+    res.send(updatedType)
+  }catch(error){
+    console.log(error.message)
+    res.status(500).json({
+      message:"Internal Server Error"
+    })
+  }
+});
+
+
+/* Ajout type */
+router.put('/', async function(req, res, next) {
+  try{
+    const {name, description} = req.body
+    
+    const newType = await prisma.type.create({
+      data:{
+        name,
+        description
+      }
+    })
+    
+    res.send(newType)
+  }catch(error){
+    console.log(error.message)
+    res.status(500).json({
+      message:"Internal Server Error"
+    })
+  }
+});
+
+/* Suppression type */
+router.delete('/:id', async function(req, res, next) {
+  try{
+    const id = parseInt(req.params.id)
+    const deleteType = await prisma.type.delete({
+      where:{
+        id:id
+      }
+    })
+    res.send(deleteType)
+  }catch(error){
+    console.log(error.message)
+    res.status(500).json({
+      message:"Internal Server Error"
+    })
+  }
+});
 module.exports = router;
