b386f52b6bc587bd202aaed05b316d063a789cdc
Adem <ademduran6888@gmail.com>
2023-05-08 20:16:29 +0200

    refactor(classes): making groups by route

--- routes/classes.js
+++ routes/classes.js
@@ -3,98 +3,94 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 var router = express.Router();
 
-/* GET types listing. */
-router.get('/', async function(req, res, next) {
-  try{
-    const classes = await prisma.class.findMany()
-    res.send(classes)
-  }catch(error){
-    console.log(error.message)
-    res.status(500).json({
-      message:"Internal Server Error"
-    })
-  }
-});
+router.route('/')
+  .get(async function(req, res, next) {
+    /* Affichage complet */
+    try {
+      const classes = await prisma.class.findMany();
+      res.send(classes);
+    } catch(error) {
+      console.log(error.message);
+      res.status(500).json({
+        message:"Internal Server Error"
+      });
+    }
+  })
+  .post(async function(req, res, next) {
+    /* Ajout Classe */
+    try {
+      const { name, description } = req.body;
+      const newClasse = await prisma.class.create({
+        data: {
+          name,
+          description,
+        },
+      })
+      res.send(newClasse);
+    } catch(error) {
+      console.log(error.message);
+      res.status(500).json({
+        message:"Internal Server Error"
+      });
+    }
+  });
 
-/* Affichage unitaire classe */
-router.get('/:id', async function(req, res, next) {
-  try{
-    const id = parseInt(req.params.id)
-    const classe = await prisma.class.findUnique({
-      where:{
-        id:id
-      }
-    })
-    res.send(classe)
-  }catch(error){
-    console.log(error.message)
-    res.status(500).json({
-      message:"Internal Server Error"
-    })
-  }
-});
+router.route('/:id')
+  .get(async function(req, res, next) {
+    /* Affichage unitaire classe */
+    try {
+      const id = parseInt(req.params.id);
+      const classe = await prisma.class.findUnique({
+        where: {
+          id,
+        },
+      });
+      res.send(classe);
+    } catch(error) {
+      console.log(error.message);
+      res.status(500).json({
+        message:"Internal Server Error"
+      });
+    }
+  })
+  .put(async function(req, res, next) {
+    /* Mise à jour classe */
+    try{
+      const { name, description } = req.body;
+      const id = parseInt(req.params.id);
+      const updatedClasse = await prisma.class.update({
+        where: {
+          id,
+        },
+        data: {
+          name,
+          description,
+        },
+      })
+      res.send(updatedClasse);
+    } catch(error) {
+      console.log(error.message);
+      res.status(500).json({
+        message:"Internal Server Error"
+      });
+    }
+  })
+  .delete(async function(req, res, next) {
+    /* Suppression type */
+    try{
+      const id = parseInt(req.params.id);
+      const deleteClasse = await prisma.class.delete({
+        where: {
+          id,
+        },
+      });
+      res.send(deleteClasse);
+    } catch(error) {
+      console.log(error.message);
+      res.status(500).json({
+        message:"Internal Server Error"
+      });
+    }
+  });
 
-/* Mise à jour classe */
-router.post('/:id', async function(req, res, next) {
-  try{
-    const {name, description} = req.body
-    const id = parseInt(req.params.id)
-    const updatedClasse = await prisma.class.update({
-      where:{
-        id:id
-      },
-      data:{
-        name,
-        description
-      }
-    })
-    
-    res.send(updatedClasse)
-  }catch(error){
-    console.log(error.message)
-    res.status(500).json({
-      message:"Internal Server Error"
-    })
-  }
-});
-
-
-/* Ajout Classe */
-router.put('/', async function(req, res, next) {
-  try{
-    const {name, description} = req.body
-    
-    const newClasse = await prisma.class.create({
-      data:{
-        name,
-        description
-      }
-    })
-    
-    res.send(newClasse)
-  }catch(error){
-    console.log(error.message)
-    res.status(500).json({
-      message:"Internal Server Error"
-    })
-  }
-});
-
-/* Suppression type */
-router.delete('/:id', async function(req, res, next) {
-  try{
-    const id = parseInt(req.params.id)
-    const deleteClasse = await prisma.class.delete({
-      where:{
-        id:id
-      }
-    })
-    res.send(deleteClasse)
-  }catch(error){
-    console.log(error.message)
-    res.status(500).json({
-      message:"Internal Server Error"
-    })
-  }
-});
 module.exports = router;
