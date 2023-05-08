1eb8a31c45537cd343625eb02b66e6b781e1c510
Adem <ademduran6888@gmail.com>
2023-05-09 00:00:44 +0200

    feat(app): adding cors module

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var cors = require('cors')
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -11,6 +12,7 @@ var abilitiesRouter = require('./routes/abilities');
 
 var app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
