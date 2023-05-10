77268e3e796f1434450deb017253a1c31753e79c
Adem <ademduran6888@gmail.com>
2023-05-10 16:04:25 +0200

    refactor(cards): linting and refactoring code

--- routes/cards.js
+++ routes/cards.js
@@ -3,127 +3,150 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 var router = express.Router();
 
-/* GET ability listing. */
 router.route('/')
   .get(async function(req, res, next) {
-    try{
-      const cards = await prisma.card.findMany()
-      res.send(cards)
-    }catch(error){
-      console.log(error.message)
+    /* Affichage complet */
+    try {
+      const cards = await prisma.card.findMany();
+      res.send(cards);
+    } catch(error) {
+      console.log(error.message);
       res.status(500).json({
         message:"Internal Server Error"
-      })
+      });
     }
   })
-/* Ajout card */
   .post(async function(req, res, next) {
-    try{
+    /* Ajout card */
+    try {
       const {
-        name, surname,illustration, typeId, classId, power,abilityIdActive1, costId1Active1,costId2Active1, abilityIdActive2, costId1Active2,costId2Active2,
-        abilityIdPassive1, conditionId
-      } = req.body
+        name,
+        surname,
+        illustration,
+        typeId,
+        classId,
+        power,
+        abilityIdActive1,
+        costId1Active1,
+        costId2Active1,
+        abilityIdActive2,
+        costId1Active2,
+        costId2Active2,
+        abilityIdPassive1,
+        conditionId,
+      } = req.body;
       
-      // Ajout et teste des capacités passive 
+      // Ajout et test des capacités passives
       const passive = await prisma.passive.findFirst({
-        where:{
-          abilityId:abilityIdPassive1,
-          conditionId
+        where: {
+          abilityId: abilityIdPassive1,
+          conditionId,
         }
-      })
-      if(!passive){
+      });
+
+      if (!passive) {
         const newPassive = await prisma.passive.create({
           data:{
-            abilityId:abilityIdPassive1,
+            abilityId: abilityIdPassive1,
             conditionId
           }
-        })
+        });
       }
+
       // Ajout et teste des capacités active 1 et 2 
       const active1 = await prisma.active.findFirst({
-        where:{
-          abilityId:abilityIdActive1,
+        where: {
+          abilityId: abilityIdActive1,
         }
-      })
-      if(!active1){
+      });
+
+      if (!active1) {
         const newActive = await prisma.active.create({
           data:{
-            abilityId:abilityIdActive1,
-          }
-        })
-      }
-      const active2 = await prisma.active.findFirst({
-        where:{
-          abilityId:abilityIdActive2,
-        }
-      })
-      if(!active2 && abilityIdActive2){
-        const newActive2 = await prisma.active.create({
-          data:{
-            abilityId:abilityIdActive2,
+            abilityId: abilityIdActive1,
           }
         })
       }
 
+      const active2 = await prisma.active.findFirst({
+        where: {
+          abilityId: abilityIdActive2,
+        }
+      });
+
+      if (!active2 && abilityIdActive2) {
+        const newActive2 = await prisma.active.create({
+          data: {
+            abilityId:abilityIdActive2,
+          }
+        });
+      }
+
       // Ajout et teste CostOnActive
       const cost1OnActive1 = await prisma.costOnActive.findFirst({
-        where:{
-          idActive:active1.id || newActive1.id,
-          idCost:costId1Active1
+        where: {
+          idActive: active1.id || newActive1.id,
+          idCost: costId1Active1,
         }
-      })
-      if(!cost1OnActive1){
+      });
+
+      if (!cost1OnActive1) {
         const newCost1OnActive1 = await prisma.costOnActive.create({
-          data:{
-            idActive:active1.id || newActive1.id,
-            idCost:costId1Active1
+          data: {
+            idActive: active1.id || newActive1.id,
+            idCost: costId1Active1,
           }
-        })
+        });
       }
+
       const cost2OnActive1 = await prisma.costOnActive.findFirst({
-        where:{
-          idActive:active1.id || newActive1.id,
-          idCost:costId2Active1
+        where: {
+          idActive: active1.id || newActive1.id,
+          idCost: costId2Active1,
         }
-      })
-      if(!cost2OnActive1 && costId2Active1){
+      });
+
+      if (!cost2OnActive1 && costId2Active1) {
         const newCost2OnActive1 = await prisma.costOnActive.create({
-          data:{
-            idActive:active1.id || newActive1.id,
-            idCost:costId2Active1
+          data: {
+            idActive: active1.id || newActive1.id,
+            idCost: costId2Active1,
           }
-        })
+        });
       }
+
       // Cost On Active 2 si il y a
-      
-      if(active2 || newActive2){
+      if (active2 || newActive2) {
         const cost1OnActive2 = await prisma.costOnActive.findFirst({
-          where:{
-            idActive:active2.id || newActive2.id,
-            idCost:costId1Active2
+          where: {
+            idActive: active2.id || newActive2.id,
+            idCost: costId1Active2,
           }
-        })
-        if(!cost1OnActive2){
+        });
+
+        if (!cost1OnActive2) {
           const newCost1OnActive2 = await prisma.costOnActive.create({
-            data:{
-              idActive:active2.id || newActive2.id,
-              idCost:costId1Active2
+            data: {
+              idActive: active2.id || newActive2.id,
+              idCost: costId1Active2,
             }
           })
         }
+
         const cost2OnActive2 = await prisma.costOnActive.findFirst({
-          where:{
-            idActive:active2.id || newActive2.id,
-            idCost:costId2Active2
+          where: {
+            idActive: active2.id || newActive2.id,
+            idCost: costId2Active2,
           }
-        })
-        if(!cost2OnActive2 && costId2Active2){
+        });
+
+        if (!cost2OnActive2 && costId2Active2) {
           const newCost2OnActive2 = await prisma.costOnActive.create({
-            data:{
-              idActive:active2.id || newActive2.id,
-              idCost:costId2Active2
+            data: {
+              idActive: active2.id || newActive2.id,
+              idCost: costId2Active2,
             }
-          })
+          });
         }
       }
 
@@ -136,181 +159,201 @@ router.route('/')
           power,
           classId,
           typeId,
-          passiveId: passive.id||newPassive.id
+          passiveId: passive.id||newPassive.id,
         }
       })
       
       // Ajout des actifs sur la carte
       const ActiveOnCard = await prisma.activeOnCard.create({
-        data:{
+        data: {
           idCard:newCard.id,
-          idActive: active1.id||newActive1.id
+          idActive: active1.id||newActive1.id,
         }
-      })
-      if(abilityIdActive2){
+      });
+
+      if (abilityIdActive2){
         const Active2OnCard = await prisma.activeOnCard.create({
-          data:{
+          data: {
             idCard:newCard.id,
-            idActive: active2.id || newActive2.id
+            idActive: active2.id || newActive2.id,
           }
-        })
+        });
       }
 
-      res.send()
-    }catch(error){
-      console.log(error)
+      res.send("La carte a bien été ajoutée");
+    } catch(error) {
+      console.log(error);
       res.status(500).json({
         message:"Internal Server Error"
-      })
+      });
     }
   });
 
 router.route("/:id")
-/* Affichage unitaire */
   .get(async function(req, res, next) {
-    try{
-      const id = parseInt(req.params.id)
+    /* Affichage unitaire */
+    try {
+      const id = parseInt(req.params.id);
       const card = await prisma.card.findUnique({
-        where:{
-          id:id
+        where: {
+          id,
         }
-      })
-      res.send(card)
-    }catch(error){
-      console.log(error.message)
+      });
+      res.send(card);
+    } catch(error) {
+      console.log(error.message);
       res.status(500).json({
         message:"Internal Server Error"
-      })
+      });
     }
   })
-/* Mise à jour card */
   .put(async function(req, res, next) {
-    try{
+    /* Mise à jour card */
+    try {
       const {
-        name, surname,illustration, typeId, classId, power,
-        abilityIdActive1, costId1Active1,costId2Active1, 
-        abilityIdActive2, costId1Active2,costId2Active2,
-        abilityIdPassive, conditionId
-      } = req.body
-      const id = parseInt(req.params.id)
+        name,
+        surname,
+        illustration,
+        typeId,
+        classId,
+        power,
+        abilityIdActive1,
+        costId1Active1,
+        costId2Active1, 
+        abilityIdActive2,
+        costId1Active2,
+        costId2Active2,
+        abilityIdPassive,
+        conditionId,
+      } = req.body;
+      const id = parseInt(req.params.id);
       
       // Ajout et teste des capacités passive 
       const passive = await prisma.passive.findFirst({
-        where:{
-          abilityId:abilityIdPassive,
-          conditionId
+        where: {
+          abilityId: abilityIdPassive,
+          conditionId,
         }
-      })
-      if(!passive){
+      });
+
+      if (!passive) {
         const newPassive = await prisma.passive.create({
-          data:{
-            abilityId:abilityIdPassive,
-            conditionId
+          data: {
+            abilityId: abilityIdPassive,
+            conditionId,
           }
-        })
+        });
       }
 
       // Ajout et teste des capacités active 1 et 2 
       const active1 = await prisma.active.findFirst({
-        where:{
-          abilityId:abilityIdActive1,
+        where: {
+          abilityId: abilityIdActive1,
         }
-      })
-      if(!active1){
+      });
+
+      if (!active1) {
         const newActive = await prisma.active.create({
-          data:{
-            abilityId:abilityIdActive1,
+          data: {
+            abilityId: abilityIdActive1,
           }
-        })
+        });
       }
       const active2 = await prisma.active.findFirst({
-        where:{
+        where: {
           abilityId:abilityIdActive2,
         }
-      })
-      if(!active2 && abilityIdActive2){
+      });
+
+      if (!active2 && abilityIdActive2) {
         const newActive2 = await prisma.active.create({
-          data:{
-            abilityId:abilityIdActive2,
+          data: {
+            abilityId: abilityIdActive2,
           }
-        })
+        });
       }
 
       // Ajout et teste CostOnActive
       const cost1OnActive1 = await prisma.costOnActive.findFirst({
-        where:{
-          idActive:active1.id || newActive1.id,
-          idCost:costId1Active1
+        where: {
+          idActive: active1.id || newActive1.id,
+          idCost: costId1Active1,
         }
-      })
-      if(!cost1OnActive1){
+      });
+
+      if (!cost1OnActive1) {
         const newCost1OnActive1 = await prisma.costOnActive.create({
-          data:{
-            idActive:active1.id || newActive1.id,
-            idCost:costId1Active1
+          data: {
+            idActive: active1.id || newActive1.id,
+            idCost: costId1Active1,
           }
-        })
+        });
       }
       const cost2OnActive1 = await prisma.costOnActive.findFirst({
-        where:{
-          idActive:active1.id || newActive1.id,
-          idCost:costId2Active1
+        where: {
+          idActive: active1.id || newActive1.id,
+          idCost: costId2Active1,
         }
-      })
-      if(!cost2OnActive1 && costId2Active1){
+      });
+
+      if (!cost2OnActive1 && costId2Active1) {
         const newCost2OnActive1 = await prisma.costOnActive.create({
-          data:{
-            idActive:active1.id || newActive1.id,
-            idCost:costId2Active1
+          data: {
+            idActive: active1.id || newActive1.id,
+            idCost: costId2Active1,
           }
-        })
+        });
       }
+
       // Cost On Active 2 si il y a
-      if(active2 || newActive2){
+      if (active2 || newActive2) {
         const cost1OnActive2 = await prisma.costOnActive.findFirst({
-          where:{
-            idActive:active2.id || newActive2.id,
-            idCost:costId1Active2
+          where: {
+            idActive: active2.id || newActive2.id,
+            idCost: costId1Active2
           }
-        })
-        if(!cost1OnActive2){
+        });
+
+        if (!cost1OnActive2) {
           const newCost1OnActive2 = await prisma.costOnActive.create({
-            data:{
-              idActive:active2.id || newActive2.id,
-              idCost:costId1Active2
+            data: {
+              idActive: active2.id || newActive2.id,
+              idCost: costId1Active2,
             }
-          })
+          });
         }
         const cost2OnActive2 = await prisma.costOnActive.findFirst({
-          where:{
-            idActive:active2.id || newActive2.id,
-            idCost:costId2Active2
+          where: {
+            idActive: active2.id || newActive2.id,
+            idCost: costId2Active2,
           }
-        })
-        if(!cost2OnActive2 && costId2Active2){
+        });
+
+        if (!cost2OnActive2 && costId2Active2) {
           const newCost2OnActive2 = await prisma.costOnActive.create({
-            data:{
-              idActive:active2.id || newActive2.id,
-              idCost:costId2Active2
+            data: {
+              idActive: active2.id || newActive2.id,
+              idCost: costId2Active2,
             }
-          })
+          });
         }
       }
+
       // Mise à jour de la carte
       const updatedCard = await prisma.card.update({
-        where:{
-          id:id
+        where: {
+          id,
         },
-        data:{
+        data: {
           name,
           surname,
           illustration,
           power,
           classId,
           typeId,
-          passiveId: passive.id || newPassive.id
-        }
-      })
+          passiveId: passive.id || newPassive.id,
+        },
+      });
       
       // Ajout des actifs sur la carte
       const deleteActiveOnCard = await prisma.activeOnCard.deleteMany({
@@ -318,82 +361,84 @@ router.route("/:id")
           idCard: id,
         },
       });
+
       // Ajout des actifs sur la carte
       const ActiveOnCard = await prisma.activeOnCard.create({
-        data:{
+        data: {
           idCard:id,
-          idActive: active1.id||newActive1.id
+          idActive: active1.id||newActive1.id,
         }
-      })
-      if(abilityIdActive2){
+      });
+
+      if (abilityIdActive2) {
         const Active2OnCard = await prisma.activeOnCard.create({
-          data:{
+          data: {
             idCard:id,
-            idActive: active2.id || newActive2.id
+            idActive: active2.id || newActive2.id,
           }
-        })
+        });
       }    
-      res.send()
-    }catch(error){
-      console.log(error)
+      res.send("La carte a bien été modifiée");
+    } catch(error) {
+      console.log(error);
       res.status(500).json({
         message:"Internal Server Error"
-      })
+      });
     }
   })
-
-  
-/* Suppression ability */
   .delete(async function(req, res, next) {
-    try{
-      const id = parseInt(req.params.id)
+    /* Suppression ability */
+    try {
+      const id = parseInt(req.params.id);
       const deleteActiveOnCard = await prisma.activeOnCard.deleteMany({
-        where:{
-          idCard:id
+        where: {
+          idCard:id,
         }
-      })
+      });
       const deleteCard = await prisma.card.delete({
-        where:{
-          id:id
+        where: {
+          id,
         }
-      })
-      res.send(deleteCard)
-    }catch(error){
-      console.log(error.message)
+      });
+      res.send(deleteCard);
+    } catch(error) {
+      console.log(error.message);
       res.status(500).json({
         message:"Internal Server Error"
-      })
+      });
     }
   });
-router.get('/name-generator', async function(req, res, next) {
-  try {
-    const names = await prisma.name.findMany();
-    const random = Math.floor(Math.random() * names.length) + 1;
-    const randomName = await prisma.name.findUnique({
-      where: {
-        id: random,
-      }
-    });
 
-    const surnames = await prisma.surname.findMany();
-    const randombis = Math.floor(Math.random() * surnames.length) + 1;
-    const randomSurname = await prisma.surname.findUnique({
-      where: {
-        id: randombis,
-      },
-    });
+router.route('/name-generator')
+  .get(async function(req, res, next) {
+    try {
+      const names = await prisma.name.findMany();
+      const random = Math.floor(Math.random() * names.length) + 1;
+      const randomName = await prisma.name.findUnique({
+        where: {
+          id: random,
+        }
+      });
 
-    res.send({
-      name: randomName.name,
-      surname: randomSurname.name,
-    });
-  } catch(error) {
-    console.log(error.message)
-    res.status(500).json({
-      message:"Internal Server Error"
-    })
-  }
-});
+      const surnames = await prisma.surname.findMany();
+      const randombis = Math.floor(Math.random() * surnames.length) + 1;
+      const randomSurname = await prisma.surname.findUnique({
+        where: {
+          id: randombis,
+        },
+      });
+
+      res.send({
+        name: randomName.name,
+        surname: randomSurname.name,
+      });
+    } catch(error) {
+      console.log(error.message);
+      res.status(500).json({
+        message:"Internal Server Error"
+      });
+    }
+  });
 
 module.exports = router;
 
